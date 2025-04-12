9fb2dbdbdfb749acb4fc5836b0c9787d10b2d981
bzee2004 <brianzee2004@gmail.com>
2025-04-11 18:59:20 -0700

    footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,36 @@
-
+import { Link } from "react-router-dom"
+import "./Footer.css"
 
 export default function Footer() {
     return (
-        <div>
-            footer
+        <div className="footer">
+
+            <div className="main-content">
+                <div className="help-links">
+                    <h1>Help</h1>
+                    <Link className="link" to="">Contact Us</Link>
+                    <Link className="link" to="">FAQ</Link>
+                </div>
+                <div className="subscribe">
+                    <h1>Subscribe</h1>
+                    <p>Get reminders about any new products and receive our daily newsletter</p>
+                    <form action="POST" onSubmit={(e) => e.preventDefault()}>
+                        <input type="email" name="email" id="email" placeholder="Email Address"/>
+                        <input type="submit" />
+                    </form>
+                </div>
+                <div className="social-media">
+                    <h1>Follow us:</h1>
+                    <div className="social-media-links">
+                        <a href="/" target="_blank" className="social-media-link"></a>
+                        <a href="/" target="_blank" className="social-media-link"></a>
+                        <a href="/" target="_blank" className="social-media-link"></a>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <p>Copyright 2025 - CPSC362 Group 17</p>
         </div>
     )
 }
