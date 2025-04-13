7c520b06cf59dbcc37cd366475b0ea478cadbd5b
bzee2004 <brianzee2004@gmail.com>
2025-04-13 02:12:03 -0700

    fixed envs for vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    "/api": {
+      target: "http://localhost:1337",
+      changeOrigin: true,
+      secure: false
+    }
+  }
 })
