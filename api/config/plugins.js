f25c3ae296f6eaded0a020b47f4731536ea038e0
bzee2004 <brianzee2004@gmail.com>
2025-04-17 05:08:21 -0700

    added aws s3 for images

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -1,1 +1,24 @@
-module.exports = () => ({});
+module.exports = ({ env }) => {
+  
+    return {
+      upload: {
+        config: {
+          provider: 'aws-s3',
+          providerOptions: {
+            region: env('AWS_REGION'),
+            credentials: {
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_ACCESS_SECRET'),
+            },
+            params: {
+              Bucket: env('AWS_BUCKET'),
+            },
+            baseUrl: env('AWS_BASE_URL')
+          },
+        breakpoints: {},
+        responsiveDimensions: false,
+        },
+      },
+    };
+  };
+  
