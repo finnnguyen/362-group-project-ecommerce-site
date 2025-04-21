3d0d2999b5c0f6c8bf62a515960d3e96d9c9bf3b
bzee2004 <brianzee2004@gmail.com>
2025-04-21 02:25:07 -0700

    updated cors to cover localhost

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -4,7 +4,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'https://tiedandtrue.vercel.app'],
+      origin: ['http:/127.0.0.1:5173', 'http://localhost:5173', 'https://tiedandtrue.vercel.app'],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       headers: ["Content-Type", "Authorization"],
       credentials: true
