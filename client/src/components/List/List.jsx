00e6c2d019ed39d179fe8fc9e73af4c6c27f6d74
bzee2004 <brianzee2004@gmail.com>
2025-04-22 14:37:33 -0700

    added list.jsx

---
+++ client/src/components/List/List.jsx
@@ -0,0 +1,23 @@
+import img1 from "../../public/jack-black.webp";
+
+export const list = () => {
+
+    const data = [
+        {
+            id: 1,
+            img: img1,
+            title: "Test tie",
+            isNew: true,
+            price: 12 
+        },
+        {
+            id: 2,
+            img: img1,
+            title: "Test bowtie",
+            isNew: false,
+            price: 10
+        }
+    ]
+
+
+}
